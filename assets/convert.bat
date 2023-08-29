eeadf0e369884b4ee4cadb4230825e750497edda
Avremi Friedman <36522808+hishtadlut@users.noreply.github.com>
2023-08-29 18:36:11 +0300

    jpg and jpeg to webp

---
+++ assets/convert.bat
@@ -0,0 +1,27 @@
+@echo off
+setlocal enabledelayedexpansion
+
+cd assets
+
+set "webp_count=0"
+set "avif_count=0"
+set "webp_skipped=0"
+set "avif_skipped=0"
+
+echo Converting JPEG to WebP...
+for %%i in (*.webp) do (
+    set "webp_output=%%~ni.webp"
+    if exist !webp_output! (
+        echo Skipped: !webp_output! already exists.
+        set /a webp_skipped+=1
+    ) else (
+        D:\gptq\manaralodge\.git\libwebp-1.3.1\libwebp-1.3.1-windows-x64\bin\cwebp -q 80 %%i -o !webp_output!
+        echo Converted: %%i to !webp_output!
+        set /a webp_count+=1
+    )
+)
+
+echo Done!
+echo WebP: Converted !webp_count! files, skipped !webp_skipped! files.
+
+endlocal
